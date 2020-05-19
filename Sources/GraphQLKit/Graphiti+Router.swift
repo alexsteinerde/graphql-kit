db30c2c26c9cc785df4d3c10a5c6226e14cb7554
maximkrouk <id.maximkrouk@gmail.com>
2020-05-20 02:37:38 +0300

    Added support for middleware injection
    Moved registration method from Application to RoutesBuilder

--- Sources/GraphQLKit/Graphiti+Router.swift
+++ Sources/GraphQLKit/Graphiti+Router.swift
@@ -2,7 +2,7 @@ import Vapor
 import Graphiti
 import GraphQL
 
-extension Application {
+extension RoutesBuilder {
     public func register<RootType: FieldKeyProvider>(graphQLSchema schema: Schema<RootType, Request>, withResolver rootAPI: RootType, at path: PathComponent="graphql") {
         self.post(path) { (request) -> EventLoopFuture<Response> in
             try request.resolveByBody(graphQLSchema: schema, with: rootAPI)
