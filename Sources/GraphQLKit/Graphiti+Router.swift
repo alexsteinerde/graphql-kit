df807d274fa203a40023bc5dfd5c6be1f419887c
Jared Henderson <jaredh159@users.noreply.github.com>
2022-05-06 10:13:32 -0400

    add register overload for variadic PathComponent

--- Sources/GraphQLKit/Graphiti+Router.swift
+++ Sources/GraphQLKit/Graphiti+Router.swift
@@ -1,20 +1,37 @@
-import Vapor
 import Graphiti
 import GraphQL
+import Vapor
 
-extension RoutesBuilder {
-    public func register<RootType>(graphQLSchema schema: Schema<RootType, Request>, withResolver rootAPI: RootType, at path: PathComponent="graphql", postBodyStreamStrategy: HTTPBodyStreamStrategy = .collect) {
-        self.on(.POST, path, body: postBodyStreamStrategy) { (request) -> EventLoopFuture<Response> in
+public extension RoutesBuilder {
+    func register<RootType>(
+        graphQLSchema schema: Schema<RootType, Request>,
+        withResolver rootAPI: RootType,
+        at path: PathComponent = "graphql",
+        postBodyStreamStrategy: HTTPBodyStreamStrategy = .collect
+    ) {
+        on(.POST, path, body: postBodyStreamStrategy) { request -> EventLoopFuture<Response> in
             try request.resolveByBody(graphQLSchema: schema, with: rootAPI)
-                .flatMap({
-                    $0.encodeResponse(status: .ok, for: request)
-                })
+                .flatMap { $0.encodeResponse(status: .ok, for: request) }
         }
-        self.get(path) { (request) -> EventLoopFuture<Response> in
+        get(path) { request -> EventLoopFuture<Response> in
             try request.resolveByQueryParameters(graphQLSchema: schema, with: rootAPI)
-                .flatMap({
-                    $0.encodeResponse(status: .ok, for: request)
-                })
+                .flatMap { $0.encodeResponse(status: .ok, for: request) }
+        }
+    }
+
+    func register<RootType>(
+        graphQLSchema schema: Schema<RootType, Request>,
+        withResolver rootAPI: RootType,
+        at path: PathComponent...,
+        postBodyStreamStrategy: HTTPBodyStreamStrategy = .collect
+    ) {
+        on(.POST, path, body: postBodyStreamStrategy) { request -> EventLoopFuture<Response> in
+            try request.resolveByBody(graphQLSchema: schema, with: rootAPI)
+                .flatMap { $0.encodeResponse(status: .ok, for: request) }
+        }
+        get(path) { request -> EventLoopFuture<Response> in
+            try request.resolveByQueryParameters(graphQLSchema: schema, with: rootAPI)
+                .flatMap { $0.encodeResponse(status: .ok, for: request) }
         }
     }
 }
@@ -23,4 +40,4 @@ enum GraphQLResolveError: Swift.Error {
     case noQueryFound
 }
 
-extension GraphQLResult: Content { }
+extension GraphQLResult: Content {}
